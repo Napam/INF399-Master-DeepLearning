29c7852dfa046450bd431256589514b140bdf488
Naphat Amundsen <nam012@ii0230970.klientdrift.uib.no>
2020-10-20 19:48:37 +0200

    More on PyTorch in tutorial notebook

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker run -it -v $(pwd)/volume:/app -p 5555:8888 --rm --gpus all nam012-${1:-th}
+docker run -it -v $(pwd)/volume:/app -p 5555:8888 --ipc=host --rm --gpus all nam012-${1:-th}
+
