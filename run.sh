49f361ecfb1b24b5954d3fac6fffe0266676d4ba
Naphat Amundsen <naphat@live.no>
2020-10-21 13:10:54 +0200

    More sophisticated bash script to run docker image

--- run.sh
+++ run.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
 
-docker run -it -v $(pwd)/volume:/app -p 5555:8888 --ipc=host --rm --gpus all nam012-${1:-th}
+DOCKER_FLAGS=""
+DOCKER_COMMAND=""
+
+error() {
+	echo "u do sumting wong"
+}
+
+while getopts "dj" option; do
+	case $option in
+		d) DOCKER_FLAGS+="-d";;
+		j) DOCKER_COMMAND+="./jbook.sh";; 
+		*) error; exit;;	
+	esac
+done
+
+ARG1=${@:$OPTIND:1}
+
+docker run ${DOCKER_FLAGS} -it -v $(pwd)/volume:/app -p 5555:8888 \
+	--ipc=host --rm --gpus all nam012-${ARG1:-th} ${DOCKER_COMMAND}
 
