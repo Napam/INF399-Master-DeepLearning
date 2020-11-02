55191806b618d5e433941cc78951f6c3c5b3e4ac
Naphat Amundsen <naphat@live.no>
2020-11-02 15:54:48 +0100

    Use relative path instead for custom css

--- volume/jbook.sh
+++ volume/jbook.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 mkdir -p $HOME/.jupyter/custom
-cp /app/.jupyter/custom/custom.css $HOME/.jupyter/custom/
+cp .jupyter/custom/custom.css $HOME/.jupyter/custom/
 
 jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --config='.jupyter/jupyter_notebook_config.py' --allow-root
