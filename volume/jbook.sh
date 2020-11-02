ecd87ec3e8564f5cb70cf4ca95c309796f225a64
Naphat Amundsen <naphat@live.no>
2020-11-02 15:52:13 +0100

    Made custom .css work, took only 2 hours

--- volume/jbook.sh
+++ volume/jbook.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --config='.jupyter/jupyter_notebook_config.json' --allow-root
+mkdir -p $HOME/.jupyter/custom
+cp /app/.jupyter/custom/custom.css $HOME/.jupyter/custom/
+
+jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --config='.jupyter/jupyter_notebook_config.py' --allow-root
