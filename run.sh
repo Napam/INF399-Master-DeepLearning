7d99eb10695d5cb533f31c7c7689773dc1ac480f
Naphat Amundsen <naphat@live.no>
2020-11-02 13:50:04 +0100

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
-
 DOCKER_FLAGS=""
-DOCKER_COMMAND=""
+GPU="all"
+ARGS=""
+FRAMEWORK="th"
 
 error() {
 	echo "u do sumting wong"
 }
 
-while getopts "dj" option; do
+while getopts "djpg:f:" option; do
 	case $option in
 		d) DOCKER_FLAGS+="-d";;
-		j) DOCKER_COMMAND+="./jbook.sh";; 
+		g) GPU=${OPTARG};;
+		j) ARGS+="./jbook.sh ";; 
+		f) FRAMEWORK=${OPTARG};;
+		p) DOCKER_FLAGS+=" --publish 5555:8888";;
 		*) error; exit;;	
 	esac
 done
 
-ARG1=${@:$OPTIND:1}
+# $@ is an array or something, start at $OPTIND and rest
+ARGS+=${@:$OPTIND}
 
-docker run ${DOCKER_FLAGS} -it -v $(pwd)/volume:/app -p 5555:8888 \
-	--ipc=host --rm --gpus all nam012-${ARG1:-th} ${DOCKER_COMMAND}
+
+docker run ${DOCKER_FLAGS} -it -v $(pwd)/volume:/app \
+	--ipc=host --rm --gpus ${GPU} --name ${FRAMEWORK}-nam012-cntr nam012-${FRAMEWORK} ${ARGS}
 
