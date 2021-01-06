77dd09a7d5714f16fa36dd938309b44da936653e
Naphat Amundsen <naphat@live.no>
2021-01-06 02:02:55 +0100

    yay

---
+++ volume/download_torch.py
@@ -0,0 +1,6 @@
+import torch 
+
+torch.hub.set_dir('./torch_cache/')
+torch.hub.load_state_dict_from_url(
+    url='https://dl.fbaipublicfiles.com/detr/detr_demo-da2a99e9.pth',
+    map_location='cpu', check_hash=True)
